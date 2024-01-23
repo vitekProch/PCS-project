d0dee0dc3c05b7ab3e8d60a200b9645bc540cfd7
Vitalij Procházka <vitek.prochazka1@seznam.cz>
2024-01-23 21:07:13 +0100

    message navbar update

--- js/messages.js
+++ js/messages.js
@@ -1,15 +1,26 @@
-// <----------- MESSAGE NAV FUNKČNÍ ----------->
 const messagesNavbarToggle = document.getElementById("messages-nav-toggler");
-const messagesNavbarMenu = document.querySelector("#messages-nav");
-const messagesNavbarLinksContainer = document.getElementById("messages-nav__menu");
+const messagesNavbarMenu = document.getElementById("messages-nav");
 let isMessageNavbarExpanded = messagesNavbarToggle.getAttribute("aria-expanded") === "true";
 
 const toggleMessageNavbarVisibility = () => {
   isMessageNavbarExpanded = !isMessageNavbarExpanded;
-  messagesNavbarToggle.setAttribute("aria-expanded", isMessageNavbarExpanded);
+  messagesNavbarToggle.setAttribute("aria-expanded", isMessageNavbarExpanded.toString());
+  updateNavbarVisibility();
   messagesNavbarToggle.classList.toggle("collapsed");
 };
- 
+
+const updateNavbarVisibility = () => {
+  if (window.innerWidth <= 400) {
+    messagesNavbarMenu.style.display = isMessageNavbarExpanded ? "block" : "none";
+  } else {
+    messagesNavbarMenu.style.display = "block";
+  }
+};
+
 messagesNavbarToggle.addEventListener("click", toggleMessageNavbarVisibility);
-messagesNavbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
-messagesNavbarMenu.addEventListener("click", toggleMessageNavbarVisibility);
+
+window.addEventListener("resize", () => {
+  updateNavbarVisibility();
+});
+
+updateNavbarVisibility();
--- views/partials/navbar.view.php
+++ views/partials/navbar.view.php
@@ -11,70 +11,68 @@
         </div>
         </button>
         <div id="navbar-menu" aria-labelledby="navbar-toggle">
-<ul class="navbar-links">
-    <li class="navbar-item"><a class="navbar-link" href="<?= $GLOBALS['__BASE_PATH__']?>">Úvod</a></li>
-    <li class="navbar-item"><a class="navbar-link" href="<?= $GLOBALS['__BASE_PATH__']?>articles">Články</a></li>
-
-    <?php if(App\Services\Auth::getUser()): ?>
-        <li class="navbar-item">  
-            <div class="user-menu__container">                
-                <div class="user-menu" id="user-menu">
-                    <img id="avatar_user_menu" class="avatar" src="<?= $GLOBALS['__BASE_PATH__']?>images/avatars/<?= App\Services\Auth::getUser()['user_avatar'] ?>"></img>
-                    <div class="user-menu__user-info">                        
-                        <p class="user-menu__user-name"><?= App\Services\Auth::getUser()['user_name'] ?><span class="user-menu__role"><?= App\Services\Auth::getUser()['user_role'] ?></span></p>  
-                        <div class="email-text"><?= App\Services\Auth::getUser()['user_email'] ?></div>
-                    </div>
-                </div>
-
-                <ul class="user-dropdown" id="user-menu-dropdown">
-                    <li class="user-dropdown__separator">
-                        <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>user?user_id=<?= App\Services\Auth::getUser()['user_id'] ?>">
-                            <img class="user-dropdown__logo" src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/settings.png" alt="Nastavení">
-                            Nastavení profilu
-                        </a>
-                    </li>
-
-                    <?php if (in_array(App\Services\Auth::getUser()['user_role'], App\Services\Auth::ROLES['EDITOR'])): ?>
-                        <li>
-                            <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>articles/user-articles/to-approve" class="item__group">
-                                <div class="small-notification" id="check-articles-count" current-count="<?= App\Services\UserNotification::getToApproveCount(); ?>">
-                                    <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/check-broken2.png" alt="Nastavení">
+            <ul class="navbar-links">
+                <li class="navbar-item"><a class="navbar-link" href="<?= $GLOBALS['__BASE_PATH__']?>">Úvod</a></li>
+                <li class="navbar-item"><a class="navbar-link" href="<?= $GLOBALS['__BASE_PATH__']?>articles">Články</a></li>
+                <?php if(App\Services\Auth::getUser()): ?>
+                    <li class="navbar-item">  
+                        <div class="user-menu__container">                
+                            <div class="user-menu" id="user-menu">
+                                <img id="avatar_user_menu" class="avatar" src="<?= $GLOBALS['__BASE_PATH__']?>images/avatars/<?= App\Services\Auth::getUser()['user_avatar'] ?>"></img>
+                                <div class="user-menu__user-info">                        
+                                    <p class="user-menu__user-name"><?= App\Services\Auth::getUser()['user_name'] ?><span class="user-menu__role"><?= App\Services\Auth::getUser()['user_role'] ?></span></p>  
+                                    <div class="email-text"><?= App\Services\Auth::getUser()['user_email'] ?></div>
                                 </div>
-                                Schválit
-                            </a>
-                        </li>
-                    <?php endif; ?>
+                            </div>
 
-                    <li>
-                        <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>articles/user-articles/published" class="item__group">
-                            <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/file-edit-02.png" alt="Nastavení">
-                            Moje články
-                        </a>
+                            <ul class="user-dropdown" id="user-menu-dropdown">
+                                <li class="user-dropdown__separator">
+                                    <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>user?user_id=<?= App\Services\Auth::getUser()['user_id'] ?>">
+                                        <img class="user-dropdown__logo" src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/settings.png" alt="Nastavení">
+                                        Nastavení profilu
+                                    </a>
+                                </li>
+
+                                <?php if (in_array(App\Services\Auth::getUser()['user_role'], App\Services\Auth::ROLES['EDITOR'])): ?>
+                                    <li>
+                                        <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>articles/user-articles/to-approve" class="item__group">
+                                            <div class="small-notification" id="check-articles-count" current-count="<?= App\Services\UserNotification::getToApproveCount(); ?>">
+                                                <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/check-broken2.png" alt="Nastavení">
+                                            </div>
+                                            Schválit
+                                        </a>
+                                    </li>
+                                <?php endif; ?>
+
+                                <li>
+                                    <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>articles/user-articles/published" class="item__group">
+                                        <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/file-edit-02.png" alt="Nastavení">
+                                        Moje články
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>messages" class="item__group">
+                                        <div class="small-notification" id="message-count" current-count="<?= App\Services\UserNotification::getMessagesCount(); ?>">
+                                            <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/message-text-01.png" alt="Nastavení">
+                                        </div>  
+                                        Zprávy
+                                    </a>
+                                </li>
+
+                                <li class="user-dropdown__relative-position">
+                                    <a class="user-dropdown__item logout user-dropdown__separator" href="<?= $GLOBALS['__BASE_PATH__']?>logout" class="item__group user-dropdown__separator">
+                                        <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/sign-out.png" alt="Nastavení">
+                                        Odhlásit se
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
-
-                    <li>
-                        <a class="user-dropdown__item" href="<?= $GLOBALS['__BASE_PATH__']?>messages" class="item__group">
-                            <div class="small-notification" id="message-count" current-count="<?= App\Services\UserNotification::getMessagesCount(); ?>">
-                                <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/message-text-01.png" alt="Nastavení">
-                            </div>  
-                            Zprávy
-                        </a>
-                    </li>
-
-                    <li class="user-dropdown__relative-position">
-                        <a class="user-dropdown__item logout user-dropdown__separator" href="<?= $GLOBALS['__BASE_PATH__']?>logout" class="item__group user-dropdown__separator">
-                            <img src="<?= $GLOBALS['__BASE_PATH__']?>images/icons/sign-out.png" alt="Nastavení">
-                            Odhlásit se
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-    <?php else: ?>
-        <li class="navbar-item"><button onclick="location.href='<?= $GLOBALS['__BASE_PATH__']?>login'" class="button--success">Přihlásit se</button></li>
-    <?php endif; ?>
-</ul>
-
+                <?php else: ?>
+                    <li class="navbar-item"><button onclick="location.href='<?= $GLOBALS['__BASE_PATH__']?>login'" class="button--success">Přihlásit se</button></li>
+                <?php endif; ?>
+            </ul>
         </div>
     </nav>
 </header>
