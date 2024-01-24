938e4fbff459fe1d182758e9f63073cc9e501e4c
Vitalij Procházka <vitek.prochazka1@seznam.cz>
2024-01-24 16:30:44 +0100

    ArticleController code refactor

--- app/controllers/ArticlesController.php
+++ app/controllers/ArticlesController.php
@@ -26,10 +26,10 @@ class ArticlesController
 
     public function showAllArticles(): View
     {
+        $error = $_GET['error'] ?? null;
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
         $offset = self::PAGE_ARTICLE_LIMIT * ($page - 1);
         $articleCount = $this->article->getArticlesByStatusCount(self::PUBLISHED_ARTICLES)[0]['articles_count'];
-        $error = $_GET['error'] ?? null;
 
         return View::render('articles', [
             'title' => "Články",
@@ -46,6 +46,7 @@ class ArticlesController
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
         $offset = self::PAGE_ARTICLE_LIMIT * ($page - 1);
         $articleCount = $this->article->getUserArticlesByStatusCount(self::PUBLISHED_ARTICLES, Auth::getUser()['user_id'])[0]['articles_count'];
+        
         return View::render('articles', [
             'title' => "Publikované články",
             'articles' => $this->article->getUserArticlesByStatus(Auth::getUser()['user_id'], self::PUBLISHED_ARTICLES, self::PAGE_ARTICLE_LIMIT, $offset),
@@ -60,6 +61,7 @@ class ArticlesController
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
         $offset = self::PAGE_ARTICLE_LIMIT * ($page - 1);
         $articleCount = $this->article->getUserArticlesByStatusCount(self::DENY_ARTICLES, Auth::getUser()['user_id'])[0]['articles_count'];
+        
         return View::render('articles', [
             'title' => "Zamítnuté články",
             'articles' => $this->article->getUserArticlesByStatus(Auth::getUser()['user_id'], self::DENY_ARTICLES, self::PAGE_ARTICLE_LIMIT, $offset),
@@ -74,6 +76,7 @@ class ArticlesController
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
         $offset = self::PAGE_ARTICLE_LIMIT * ($page - 1);
         $articleCount = $this->article->getUserArticlesByStatusCount(self::TO_CHECK_ARTICLES, Auth::getUser()['user_id'])[0]['articles_count'];
+        
         return View::render('articles', [
             'title' => "Kontrolované články",
             'articles' => $this->article->getUserArticlesByStatus(Auth::getUser()['user_id'], self::TO_CHECK_ARTICLES, self::PAGE_ARTICLE_LIMIT, $offset),
@@ -88,6 +91,7 @@ class ArticlesController
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
         $offset = self::PAGE_ARTICLE_LIMIT * ($page - 1);
         $articleCount = $this->article->getArticlesByStatusCount(self::TO_CHECK_ARTICLES)[0]['articles_count'];
+        
         return View::render('articles', [
             'title' => "Kontrolované články",
             'articles' => $this->article->getAllArticlesToApprove(self::PAGE_ARTICLE_LIMIT, $offset),
@@ -97,14 +101,16 @@ class ArticlesController
         ]);
     }
 
-    public function createArticle($data)
+    public function createArticle(array $data): void
     {
         if (empty($data['title']) || empty($data['subtitle']) || empty($data['article_content'])) {
-            return header('location: ' . $GLOBALS['__BASE_PATH__'] . "articles?error=input_empty");
+            header('location: ' . $GLOBALS['__BASE_PATH__'] . "articles?error=input_empty");
+            exit;
         }
 
         if(!$this->uploadHelper->articleImageValidation()) {
-            return header('location: ' . $GLOBALS['__BASE_PATH__'] . "articles?error=wrong_file");
+            header('location: ' . $GLOBALS['__BASE_PATH__'] . "articles?error=wrong_file");
+            exit;
         };
 
         $articleImgName = $this->uploadHelper->createArticleImgName();
@@ -112,7 +118,7 @@ class ArticlesController
         
         $this->uploadHelper->uploadImg($articleImgName);
         $this->article->createArticle(Auth::getUser()['user_id'], $data['title'], $data['subtitle'], $data['article_content'], $data['article_image']);
-        return header('location: ' . $GLOBALS['__BASE_PATH__'] . 'articles');
+        header('location: ' . $GLOBALS['__BASE_PATH__'] . 'articles');
     }
 
     public function fillEditArticle(): void
@@ -121,7 +127,7 @@ class ArticlesController
         echo json_encode($article[0]);
     }
 
-    public function editArticle($data)
+    public function editArticle(array $data): void
     {
         $data['article_image'] = $_FILES['article_image']['name'];
 
@@ -131,13 +137,12 @@ class ArticlesController
             $data['article_image'] = $articleImgName;
         }
         $this->article->editArticle($data['article_id'], $data['title'], $data['subtitle'], $data['article_content'], $data['article_image']);
-        return header('location: ' . $GLOBALS['__BASE_PATH__'] . 'articles');
+        header('location: ' . $GLOBALS['__BASE_PATH__'] . 'articles');
     }
 
-    public function deleteArticle()
+    public function deleteArticle(): void
     {
         $this->article->deleteArticle($_GET['article_id']);
-        return header('Location: ' . $_SERVER['HTTP_REFERER']);
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
-
 }
