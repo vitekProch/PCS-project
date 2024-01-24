9c4961b17b11c7a380f4abb00af7bd70d5d88215
Vitalij Procházka <vitek.prochazka1@seznam.cz>
2024-01-24 16:47:15 +0100

    RegisterController code refactor

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -12,30 +12,32 @@ class RegisterController
 {
     protected User $user;
 
-
     public function __construct()
     {
         $this->user = new User();
     }
+
     public function showRegister(): View
     {
         $error = $_GET['error'] ?? null;
+        
         return View::render('register', [
             'title' => "Registrace",
             'error'=> Errors::errorMessage($error),
         ]);
     }
     
-    public function registerUser($data)
+    public function registerUser(array $data): void
     {
         try {
             $this->user->registerUser($data['user_name'], $data['email'], $data['password'], $data['avatar']);
             $newUser = $this->user->exist($data['email'])[0];
             Auth::login($newUser['id'], $newUser['name'], $newUser['email'], $newUser['avatar'], $newUser['role']);
-            return header('location: ' . $GLOBALS['__BASE_PATH__']. 'articles');
+
+            header('location: ' . $GLOBALS['__BASE_PATH__']. 'articles');
         }
         catch (Exception $e) {
-            return header('location: ' . $GLOBALS['__BASE_PATH__'] . 'register?error=user_exist');
+            header('location: ' . $GLOBALS['__BASE_PATH__'] . 'register?error=user_email_exist');
         }
     }
 }
