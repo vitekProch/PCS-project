4866bd827f4b5f1d1c5fd48032d13f86710d178f
Vitalij Procházka <vitek.prochazka1@seznam.cz>
2024-01-25 19:09:16 +0100

    Code refactor

--- web/routes.php
+++ web/routes.php
@@ -11,7 +11,6 @@ use Core\Router;
 
 $router = new Router();
 
-//defince cest
 
 $router->get($GLOBALS['__BASE_PATH__'], HomepageController::class, 'showHomepage');
 
@@ -20,10 +19,14 @@ $router->post($GLOBALS['__BASE_PATH__'] . "register", RegisterController::class,
 
 $router->get($GLOBALS['__BASE_PATH__'] . "login", LoginController::class, 'showLogin');
 $router->post($GLOBALS['__BASE_PATH__'] . "login", LoginController::class, 'loginUser');
-
 $router->get($GLOBALS['__BASE_PATH__'] . "logout", LoginController::class, 'logoutUser');
 
 $router->get($GLOBALS['__BASE_PATH__'] . "articles", ArticlesController::class, 'showAllArticles');
+$router->post($GLOBALS['__BASE_PATH__'] . "articles", ArticlesController::class, 'createArticle', ["Auth:USER"]);
+
+$router->get($GLOBALS['__BASE_PATH__'] . "articles/delete", ArticlesController::class, 'deleteArticle', ["Auth:USER"]);
+$router->post($GLOBALS['__BASE_PATH__'] . "articles/edit", ArticlesController::class, 'editArticle', ["Auth:USER"]);
+$router->post($GLOBALS['__BASE_PATH__'] . "articles/fill-edit-article", ArticlesController::  class, 'fillEditArticle');
 
 $router->get($GLOBALS['__BASE_PATH__'] . "articles/user-articles/published", ArticlesController::class, 'showUserPublishedArticles', ["Auth:USER"]);
 $router->get($GLOBALS['__BASE_PATH__'] . "articles/user-articles/deny", ArticlesController::class, 'showUserDenyArticles', ["Auth:USER"]);
@@ -37,27 +40,13 @@ $router->post($GLOBALS['__BASE_PATH__'] . "article-detail/comment", ArticleDetai
 $router->get($GLOBALS['__BASE_PATH__'] . "article-detail/approval", ArticleDetailController::class, 'acceptArticle', ["Auth:EDITOR"]);
 $router->post($GLOBALS['__BASE_PATH__'] . "article-detail/approval", ArticleDetailController::class, 'denyArticle', ["Auth:EDITOR"]);
 
-$router->get($GLOBALS['__BASE_PATH__'] . "articles/delete", ArticlesController::class, 'deleteArticle', ["Auth:USER"]);
-$router->post($GLOBALS['__BASE_PATH__'] . "articles", ArticlesController::class, 'createArticle', ["Auth:USER"]);
-
-$router->post($GLOBALS['__BASE_PATH__'] . "articles/edit", ArticlesController::class, 'editArticle', ["Auth:USER"]);
-$router->post($GLOBALS['__BASE_PATH__'] . "articles/fill-edit-article", ArticlesController::  class, 'fillEditArticle');
-
-
 $router->get($GLOBALS['__BASE_PATH__'] . "user", UserController::class, 'showUser');
 $router->post($GLOBALS['__BASE_PATH__'] . "user/change-avatar", UserController::class, 'changeAvatar');
 $router->post($GLOBALS['__BASE_PATH__'] . "user/settings-edit", UserController::class, 'userSettingsEdit');
 
-
 $router->get($GLOBALS['__BASE_PATH__'] . "messages", MessagesController::class, 'showMessages', ["Auth:USER"]);
 
-
-
-
-//zjištění na jaké adrese 
 $currentUrl = $_SERVER['REQUEST_METHOD'] . $_SERVER['REQUEST_URI'];
 $currentUrl = parse_url($currentUrl)['path'];
 
-
-//spusť metodu pro tuto URL na konkrétním kontroleru
 $router->dispatch($currentUrl);
